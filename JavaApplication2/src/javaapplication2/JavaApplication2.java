8562d8f070dc1cda6253366fe7d194f7c5578676
rahmad_4114 <rahmadwidi21@gmail.com>
2017-04-07 09:50:13 +0700

    Update JavaApplication2.java

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -20,4 +20,17 @@ public class JavaApplication2 {
             System.out.print(arr[i] + " ");
         }
     }
+    public static void sumFibo(int jum){
+        int arr[] = new int[jum];
+        arr[0] = 1;
+        arr[1] = 1;
+        for(int i = 2; i < jum; i++){
+            arr[i] = arr[i-1]+arr[i-2];
+        }
+        int res = 0;
+        for(int i = 0; i < jum; i++){
+            res += arr[i];
+        }
+        System.out.println(res);
+    }
 }
